45830500c6a1870bcd82eecedba13424eba43ed1
Lorcan Powell <146140943+lorcy-p@users.noreply.github.com>
2024-03-08 16:34:16 +0000

    Scatter graph fixed. Working on function graph.

--- JavaScript.js
+++ JavaScript.js
@@ -3,7 +3,7 @@
 
 function Functionbutton() {
     var IDToRemove = ["Header", "IntroText", "FunctionText", "ScatterText", "ScatterButton", "FunctionButton"];
-    var IDToShow = ["mainMenuButton"];
+    var IDToShow = ["mainMenuButton", "functionCanvas", "inputFunction","inputfunctionButton"];
     for (var i of IDToRemove) {
         var textElement = document.getElementById(i);
         textElement.style.display = 'none';
@@ -33,7 +33,7 @@ function Scatterbutton() {
 }
 function mainMenuButton() {
     console.log("test");
-    var IDToRemove = ["scatterCanvas", "mainMenuButton" , "Y_max","X_max"];
+    var IDToRemove = ["scatterCanvas", "mainMenuButton", "Y_max", "X_max", "inputX", "inputY", "inputButton", "functionCanvas", "inputFunction", "inputfunctionButton"];
     var IDToShow = ["Header", "IntroText", "FunctionText", "ScatterText", "ScatterButton", "FunctionButton"];
     ["scatterCanvas", "mainMenuButton"];
     for (var i of IDToRemove) {
@@ -68,7 +68,7 @@ function drawScatterGraph(dataPoints) {
 
     for (var i = 0; i < dataPoints.length; i++) {
         
-        percentdataPoints[i] = dataPoints[i];
+        percentdataPoints[i] = Object.assign({}, dataPoints[i]);
         var point = percentdataPoints[i];
         var pointpercent = 0;
         pointpercent = point.x / largestx;
@@ -79,8 +79,8 @@ function drawScatterGraph(dataPoints) {
         point.y = 400 - point.y;
     }
 
-    console.log(largestx, largesty);
-
+    const context = canvas.getContext('2d');
+    context.clearRect(0, 0, canvas.width, canvas.height);
     for (var i = 0; i < dataPoints.length; i++) {
         var point = percentdataPoints[i];
         ctx.beginPath();
@@ -119,5 +119,47 @@ function addDataPoint() {
     } else {
         alert("Please enter valid X and Y coordinates");
     }
+}
 
+
+
+function drawFunctionGraph(UserFunction) {
+    var canvas = document.getElementById("functionCanvas");
+    var ctx = canvas.getContext("2d");
+
+    
+
+    ctx.fillStyle = "blue";
+    
+    const context = canvas.getContext('2d');
+    context.clearRect(0, 0, canvas.width, canvas.height);
+
+
+
+    ctx.lineWidth = 5;
+    ctx.beginPath();
+    ctx.moveTo(0, 0);
+    ctx.lineTo(0, 700);
+    ctx.stroke();
+    ctx.beginPath();
+    ctx.moveTo(0, 400);
+    ctx.lineTo(800, 400);
+    ctx.stroke();
+}
+
+function addDataPoint() {
+    var inputX = document.getElementById("inputX").value;
+    var inputY = document.getElementById("inputY").value;
+
+    if (inputX !== "" && inputY !== "") {
+        var newDataPoint = { x: parseFloat(inputX), y: parseFloat(inputY) };
+        userInputDataPoints.push(newDataPoint);
+
+        document.getElementById("inputX").value = "";
+        document.getElementById("inputY").value = "";
+
+        drawScatterGraph(userInputDataPoints);
+    } else {
+        alert("Please enter valid X and Y coordinates");
+    }
 }
